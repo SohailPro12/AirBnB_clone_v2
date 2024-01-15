11b9f01f13d2c1609dc44ab10e3b377657423aab
Mochatr <mohamedchatr8@gmail.com>
2024-01-15 03:56:12 -0800

    update

---
+++ setup_mysql_test.sql
@@ -0,0 +1,11 @@
+-- Create database.
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- Create user.
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Set the privileges
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Make changes
+FLUSH PRIVILEGES;
