7623ba44cdb62833ab43aac9625cbb6b2d9a7d46
Mochatr <mohamedchatr8@gmail.com>
2024-01-15 03:51:23 -0800

    update

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,11 @@
+-- Create database.
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- Create user.
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
+-- Set the privileges
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
+-- Make changes
 FLUSH PRIVILEGES;
