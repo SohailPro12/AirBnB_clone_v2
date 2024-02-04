da398061d9414e0a7cc5a81493f859f6dd9dea99
Sohail Charef <charefsohayl@gmail.com>
2024-02-04 07:47:36 -0800

    deploying static

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Fabric script that distributes an archive to your web servers
+using the function do_deploy
+"""
+
+from fabric.api import put, run, env
+from os.path import exists
+
+env.hosts = ['54.87.205.91', '54.87.240.9']
+
+
+def do_deploy(archive_path):
+    """distributes archive remotely"""
+    if exists(archive_path) is False:
+        return False
+    try:
+        """Extract Filename and No Extension"""
+        file_name = archive_path.split("/")[-1]
+        name = file_name.split(".")[0]
+        """Prepare Path and Upload Archive"""
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        """Create Release Directory and Extract Archive"""
+        run('mkdir -p {}{}/'.format(path, name))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, name))
+        """Clean Up Temporary Files"""
+        run('rm /tmp/{}'.format(file_n))
+        """Move Web Static Content and Remove Original Folder"""
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, name))
+        run('rm -rf {}{}/web_static'.format(path, name))
+        """Remove Old Current Link and Create New Symbolic Link"""
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, name))
+        return True
+    except Exception as e:
+        return False
