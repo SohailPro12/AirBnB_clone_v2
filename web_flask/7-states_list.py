2249eb852ef291481cfc7d26237d6f0f989f419f
Sohail Charef <charefsohayl@gmail.com>
2024-02-17 10:51:31 -0800

    update

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+ starts a Flask web application
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import state
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+"""instance of the flask class"""
+app = Flask(__name__)
+
+
+"""After each request you must remove the current SQLAlchemy Session"""
+@app.teardown_appcontext
+def teardown(exception):
+    """Closes the current SQLAlchemy session"""
+    storage.close()
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Displays a HTML page with a list of all State objects"""
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
